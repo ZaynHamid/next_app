1b85547219137010ae52b4d1c0e29c0fafb78973
Zayn Hamid <zainhamid982@gmail.com>
2024-06-14 20:24:22 +0500

    Showing Data

--- src/app/page.js
+++ src/app/page.js
@@ -8,24 +8,36 @@ import LearnLink from "@/components/learn-link";
 import connectDB from "@/lib/connectDB";
 import User from "@/models/users";
 
+const getData = async () => {
+  const res = await fetch('http://localhost:3000/api/blog/posts')
+  const data = await res.json()
+  return data
+}
+
 export default async function Home() {  
+  const {data} = await getData()
+
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       {/* <LearnLink /> */}
+
       {/* <LearUseRouter /> */}
+
       {/* <LearnServerComponent /> */}
+
       {/* {<LearnCSSModule />} */}
+
       {/* {<LearnUseImage/>} */}
+
       {/* {<FetchData />} */}
-      {/* {data.map((val,i) => (
+
+      {data.map((val,i) => (
           <div key={i}>
             <p>{val.title}</p>
             <p>{val.body}</p>
           </div>
-      ))} */}
-
-      <p>Hello World!</p>
-    
+      ))}    
     </main>
   );
 }
